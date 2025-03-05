93e0452eacfa38c291133c2d4227cdc72cde3104
danie <dani89rosa@gmail.com>
2025-03-05 17:51:13 +0100

    components

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import './Menu.css'
+function Menu() {
+    return (
+        <div className="menu">
+            <h2>Soy el componente menú</h2>
+        </div>
+    )
+}
+
+export default Menu
+//
+// rfce
+// 
