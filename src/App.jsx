8ae34368fbdebc6640e41a93f0b289d38f10a8e9
danie <dani89rosa@gmail.com>
2025-03-05 18:44:40 +0100

    onChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+
+  // crear variable reactiva con valor iniciarl, 
+  // vinculada al valor value del input y para que se actualice cada vez que se modifica, 
+  // se crea una funcion que atualice a set
+
+  const [myValue, setMyValue] = useState("");
+
+  const handleInput = (e) => {
+    console.log(e) // event hay que buscar donde está el valor de input
+    setMyValue(e.target.value)
+  }
+
   return (
     <>
-      
+      <p>{myValue}</p>
+      <input type="text" placeholder='Ej. Manolito' value={myValue} onChange={handleInput}/>
     </>
   )
 }
