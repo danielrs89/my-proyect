0e1638918a1866853c261be8176208c85eec33aa
danie <dani89rosa@gmail.com>
2025-03-05 18:18:24 +0100

    reactividad

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+
+  // desde use stage se hace reactiva
+  const [number, setNumber] = useState(0);
+
+  const addOne = ()=>{
+   setNumber(number+1);
+  }
+
   return (
     <>
-      
+      <p>Número: {number} </p>
+      <button onClick={addOne}>Añadir</button>
     </>
   )
 }
