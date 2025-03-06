e88da9bd5150bdda513542e3f4c5c02aa24ed394
danie <dani89rosa@gmail.com>
2025-03-06 11:10:23 +0100

    Renderizado condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css'
 
 function App() {
+
+  const condition = false
+
   return (
     <>
-      
+      <h3>Renderizado condicional</h3>
+
+      {/* -Con el funcionamiento del "Y lógico" [&&] */}
+      {condition && <p>La condición se cumple</p>}
+      {!condition && <p>La condición NO se cumple</p>}
+
+      {/* -Con ternarios [? () : ()] */}
+      {!condition ? (
+        <p>La condición se cumple</p>
+      ) : (
+        <p>La condición NO se cumple</p>
+      )
+      }
     </>
   )
 }
