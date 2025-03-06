d238f86ec45c9ff16a94d5179adb9812b54ff802
danie <dani89rosa@gmail.com>
2025-03-06 11:27:13 +0100

    renderizado-listas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css'
 
+const movies = [
+  "The Shawshank Redemption",
+  "The Godfather",
+  "Inception",
+  "Pulp Fiction",
+  "The Dark Knight",
+  "Forrest Gump",
+  "The Matrix",
+  "Fight Club",
+  "Interstellar",
+  "The Lord of the Rings"
+];
+
+const HTMLmovies = movies.map(movie => { return (<li key={movie}>{movie}</li>) })
+
 function App() {
   return (
     <>
-      
+      <ol>
+        {HTMLmovies}
+      </ol>
     </>
   )
 }
