9158bea576638c7e5575ecc36a75ecf229f5656b
danie <dani89rosa@gmail.com>
2025-03-10 16:47:19 +0100

    llamada api axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,46 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
-import galaxyGif from "./assets/galaxy.gif";
+import axios from "axios";
 
-// Componente funcional para mostrar la información de NASA o una animación de carga
-function HTMLNasa({ contenido }) {
-  // Si 'contenido' está disponible, mostrar la información
-  return contenido ? (
-    <div key={contenido.date}>
-      <div>
-        <h1>{contenido.title}</h1>
-        <div style={{ position: "relative", width: "100%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
-          <img src={contenido.hdurl} alt={contenido.title} style={{ maxWidth: "100%", maxHeight: "100%" }} />
-        </div>
-
-        <p>{contenido.explanation}</p>
-      </div>
-    </div>
-  ) : (
-    // Si no hay contenido, mostrar la animación de carga
-    <div style={{ position: "relative", width: "100%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
-      <img src={galaxyGif} alt="Cargando..." style={{ maxWidth: "100%", maxHeight: "100%" }} />
-    </div>
-  );
-}
+// instalar axios
+// npm install axios
 
 function App() {
-  const [contenido, setContenido] = useState(null);
-  const [cargando, setCargando] = useState(true);
 
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    // FETCH
+    // const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    // const data = await response.json();
+    // setUsers(data);
+
+    // AXIOS
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    setUsers(response.data);
+  }
+
+  // nada mas se monta el componente llama a la funcion asincrona
   useEffect(() => {
-    // Función para obtener los datos de la API de NASA
-    const llamadaApi = async () => {
-      try {
-        // Llamada a la API para obtener la foto y datos del día
-        const respuesta = await fetch(
-          "https://api.nasa.gov/planetary/apod?api_key=V2PYEh4faXJomYpYqeNxBPFo0b6WYH5nOzqh8ta9"
-        );
-        const datos = await respuesta.json();
+    getUsers();
+  }, [])
 
-        // Retrasar la actualización de estado
-        // directamente setContenido con los datos obtenidos.
-        setContenido(datos);
-        setCargando(false);
-      } catch (error) {
-        // Manejo de errores si la llamada a la API falla
-        console.log("ERROR en llamada API:", error.message);
-      }
-    };
+  //se transformad de json a elementos html
+  const HTMLgetUsers = users.map((user) => {
+    return (
+      <div key={user.id}>
+        <p>{user.name}</p>
+      </div>
+    )
+  })
 
-    // Llamada a la API al cargar el componente
-    llamadaApi();
-  }, []); // El array vacío [] asegura que la llamada se haga solo una vez al montar el componente
-
-  // Mostrar el contenido o el GIF de carga dependiendo del estado 'cargando'
-  return <HTMLNasa contenido={cargando ? null : contenido} />;
+  return (
+    <>
+    <h1>AXIOS | llamadas a apis</h1>
+      {HTMLgetUsers}
+    </>
+  )
 }
 
-export default App;
-
-
+export default App
