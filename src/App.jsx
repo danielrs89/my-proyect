841f7bbfe16f83f9c065ba3bcc569679d529a04a
danie <dani89rosa@gmail.com>
2025-03-11 11:36:38 +0100

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/HomePage'
+import BlogPage from './pages/BlogPage'
+import NavComponent from './components/NavComponent'
 
+
+// instalar dependencia router-dom 
+// npm i react-router-dom
+// SPA una pagina y muestra segun la ruta
 function App() {
   return (
     <>
-      
+      <NavComponent />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/blog' element={<BlogPage />} />
+      </Routes>
     </>
   )
 }
---
+++ src/components/NavComponent.jsx
@@ -0,0 +1,15 @@
+
+function NavComponent() {
+    return (
+        <>
+            <button>
+                <a href='/'>Home</a>
+            </button>
+            <button>
+                <a href='/blog'>Blog</a>
+            </button>
+        </>
+    )
+}
+
+export default NavComponent
